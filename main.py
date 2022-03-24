ebfba013f73b4e8e39ab2788da2a7c79f9b98958
Sr.Gambiarra <heitor.tasso@etec.sp.gov.br>
2022-03-24 17:53:04 -0300

    add widgets

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ListProperty, NumericProperty, ObjectProperty, OptionProperty 
 
 Builder.load_file('main.kv')
+
+class FloatRectangleButton(FloatLayout):
+    float = OptionProperty('left-middle', options=('left-bottom', 'left-top', 'right-middle', 'right-bottom', 'right-top'))
+    pos_y = NumericProperty(0)
+
+class FloatEllipseButton(FloatLayout):
+    pass
 
 class GamePad(BoxLayout):
 	pass
